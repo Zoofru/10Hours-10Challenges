000ab10316abd0d156c898428b7f4f429ecfd18b
Zoofru <masonbush752@gmail.com>
2021-10-04 20:12:22 -0500

    display movies work

--- movies/main.js
+++ movies/main.js
@@ -2,25 +2,33 @@ const allGen = async () => {
     const res = await axios.get('https://data-imdb1.p.rapidapi.com/genres/', {
         headers: {
             'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
-            'x-rapidapi-key': 
+            'x-rapidapi-key': 'e0d6a68fb8mshb704bbbc26e1f66p1ad819jsn2a844020c804'
         }
     })
     console.log(res.data.Genres);
-    for(let i of res.data.Genres) {
-        console.log(i.genre);
+    for(let i of res.data.Genres.splice(0, 4)) {
+        let q = 2
+        const newDivSplideCont = document.createElement('div')
+        const newGenreTitle = document.createElement('h1')
         const newDivSplide = document.createElement('div')
         const newDivSplideTrck = document.createElement('div')
         const newUL = document.createElement('ul')
+        newDivSplideCont.classList.add('splideCont')
         newDivSplide.classList.add('splide')
-        newDivSplide.setAttribute('id', 'splide')
+        newDivSplide.setAttribute('id', `splide${q}`)
         newDivSplideTrck.classList.add('splide__track')
         newUL.classList.add('splide__list')
         newUL.classList.add(i.genre)
+        newGenreTitle.innerHTML = i.genre
+        newGenreTitle.classList.add('genreTitle')
 
-        newDivSplideTrck.appendChild(newUL)
+        newDivSplideCont.appendChild(newGenreTitle)
+        newDivSplideCont.appendChild(newDivSplide)
         newDivSplide.appendChild(newDivSplideTrck)
-        document.querySelector('.caro').appendChild(newDivSplide)
+        newDivSplideTrck.appendChild(newUL)
+        document.querySelector('.caro').appendChild(newDivSplideCont)
 
+        q += 1
         getMoviesByGen(i.genre)
     }
 }
@@ -29,10 +37,11 @@ const getMoviesByGen = async genre => {
     const res = await axios.get(`https://data-imdb1.p.rapidapi.com/movie/byGen/${genre}/`, {
         headers: {
             'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
-            'x-rapidapi-key': 
+            'x-rapidapi-key': 'e0d6a68fb8mshb704bbbc26e1f66p1ad819jsn2a844020c804'
         }
     })
-    let movies = res.data.Data.splice(0, 15);
+    //spliced so it dosent return 1500 movies for each genre
+    let movies = res.data.Data.splice(0, 1);
     let movieIds = []
 
     for(let i of movies) {
@@ -48,17 +57,30 @@ const getMovieById = async id => {
     const res = await axios.get(`https://data-imdb1.p.rapidapi.com/movie/id/${id}/`, {
         headers: {
             'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
-            'x-rapidapi-key': 
+            'x-rapidapi-key': 'e0d6a68fb8mshb704bbbc26e1f66p1ad819jsn2a844020c804'
         }
     })
-    console.log(res);
+    
+    //TODO: SAME MOVIE IS LOADING. NOT ALL MOVIES ARE LOADING. (FIND BETTER WAY TO CHECK GENRE BESIDES IF ITS FIRST IN LIST. CHECK FOR DUPLICATES AND ERADICATE THEM.)
+
+    for(let i in res.data){
+        const newSlide = document.createElement('li')
+        let genreAdd = document.querySelector(`.${res.data[i].gen[0].genre}`)
+        newSlide.classList.add('splide__slide')
+        newSlide.style.backgroundImage = `url(${res.data[i].image_url})`
+        newSlide.style.backgroundSize = 'cover'
+        newSlide.classList.add('movieCover')
+
+        genreAdd.appendChild(newSlide)
+    }
+    mountSplide()
 }
 
 // getMoviesByGen('Horror')
 // allGen()
 
 
-document.addEventListener( 'DOMContentLoaded', function () {
+const mountSplide = () => {
     new Splide( '#splide', {
         fixedWidth: '15rem',
         height: '18%'
@@ -68,4 +90,4 @@ document.addEventListener( 'DOMContentLoaded', function () {
         fixedWidth: '15rem',
         height: '18%'
     }).mount()
-} );
+}
