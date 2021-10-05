526ee91a82fd49b2785818c7de5a762ffb7b4aaa
Zoofru <masonbush752@gmail.com>
2021-10-05 17:29:55 -0500

    movies added

--- movies/main.js
+++ movies/main.js
@@ -5,8 +5,9 @@ const allGen = () => {
             'x-rapidapi-key': 
         }
     }).then(res => {
-        let m = res.data.Genres.splice(0, 4)
-        let q = 2
+        let m = res.data.Genres.splice(10, 3)
+        console.log(m);
+        let q = 1
         for(let i of m) {
             const newDivSplideCont = document.createElement('div')
             const newGenreTitle = document.createElement('h1')
@@ -29,7 +30,6 @@ const allGen = () => {
             document.querySelector('.caro').appendChild(newDivSplideCont)
             
             q += 1
-            console.log(q);
             getMoviesByGen(i.genre)
         }
     })
@@ -44,25 +44,24 @@ const getMoviesByGen = genre => {
         }
     }).then(res => {
         //spliced so it dosent return 1500 movies for each genre
-        let movies = res.data.Data.splice(0, 5);
+        let movies = res.data.Data.splice(10, 15);
         let movieIds = []
-        
         for(let i in movies) {
             if(!movieIds.includes(movies[i].imdb_id)){
                 movieIds.push(movies[i].imdb_id)
             }
         }
-        setMovieIds(movieIds)
+        setMovieIds(movieIds, genre)
     })
 }
 
-const setMovieIds = movieIds => {
+const setMovieIds = (movieIds, genre) => {
     for(let i of movieIds) {
-        getMovieById(i)
+        getMovieById(i, genre)
     }
 }
 
-const getMovieById = id => {
+const getMovieById = (id, genre) => {
     const res = axios.get(`https://data-imdb1.p.rapidapi.com/movie/id/${id}/`, {
         headers: {
             'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
@@ -71,17 +70,18 @@ const getMovieById = id => {
     }).then(res => {
         for(let i in res.data){
             const newSlide = document.createElement('li')
-            let genreAdd = document.querySelector(`.${res.data[i].gen[0].genre}`)
+            let genreAdd = document.querySelector(`.${genre}`)
             newSlide.classList.add('splide__slide')
             newSlide.style.backgroundImage = `url(${res.data[i].image_url})`
             newSlide.style.backgroundSize = 'cover'
             newSlide.classList.add('movieCover')
+
             if(genreAdd != null) {
                 genreAdd.appendChild(newSlide)
             }
         }
+        mountSplide()
     })
-    mountSplide()
 }
 
 // getMoviesByGen('Horror')
@@ -89,27 +89,17 @@ allGen()
 
 
 const mountSplide = () => {
-    new Splide( '#splide', {
+    new Splide( '#splide1', {
         fixedWidth: '15rem',
         height: '18%'
     }).mount()
-    
+
     new Splide( '#splide2', {
         fixedWidth: '15rem',
         height: '18%'
     }).mount()
 
     new Splide( '#splide3', {
-        fixedWidth: '15rem',
-        height: '18%'
-    }).mount()
-
-    new Splide( '#splide4', {
-        fixedWidth: '15rem',
-        height: '18%'
-    }).mount()
-
-    new Splide( '#splide5', {
         fixedWidth: '15rem',
         height: '18%'
     }).mount()
